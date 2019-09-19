64dd1b1c4a09b3f9b5ff435fb90bb323e2532358
Makoto Yano <yan133@gmail.com>
2019-09-19 16:37:56 +0200

    Add posh-git and oh-my-posh settings to profile.ps1

---
+++ config/profile.ps1
@@ -0,0 +1,46 @@
+﻿
+Import-Module Get-ChildItemColor
+
+# Set l and ls alias to use the new Get-ChildItemColor cmdlets
+Set-Alias l Get-ChildItemColor -Option AllScope
+Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
+
+# Helper function to change directory to my development workspace
+# Change c:\ws to your usual workspace and everytime you type
+# in cws from PowerShell it will take you directly there.
+function cws { Set-Location ~\workspace }
+
+# Helper function to set location to the User Profile directory
+function cuserprofile { Set-Location ~ }
+Set-Alias ~ cuserprofile -Option AllScope
+
+# Helper function to show Unicode character
+function U
+{
+  param
+  (
+    [int] $Code
+  )
+  if ((0 -le $Code) -and ($Code -le 0xFFFF))
+  {
+    return [char] $Code
+  }
+
+  if ((0x10000 -le $Code) -and ($Code -le 0x10FFFF))
+  {
+    return [char]::ConvertFromUtf32($Code)
+  }
+  throw "Invalid character code $Code"
+}
+
+
+Import-Module posh-git
+
+# Start SshAgent if not already
+# Need this if you are using github as your remote git repository
+if (! (ps | ? { $_.Name -eq 'ssh-agent'})) {
+    Start-SshAgent
+}
+
+Import-Module oh-my-posh
+Set-Theme sorin
